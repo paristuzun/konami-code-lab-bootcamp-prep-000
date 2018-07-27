2d07adb4c33950609edd758e1c5a94bd024309bc
Paris Tuzun <paristuzun@gmail.com>
2018-07-27 05:28:07 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -17,7 +17,7 @@ function init()
 {
   var index = 0
 
-  document.body.addEventListener('keydown', function(e)
+  document.addEventListener('keydown', function(e)
   {
     const key = parseInt(e.detail || e.which)
     if (key === code[index])
