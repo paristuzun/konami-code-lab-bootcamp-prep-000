016f194bb3ffa2199fcab52f1e453c7c4a325fd9
Paris Tuzun <paristuzun@gmail.com>
2018-07-27 05:38:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,8 +14,7 @@ const codes = [
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let index = 0;
 
-function init()
-{
+function init() {
 
 
   document.addEventListener('keydown', function(e)
